610204513b4a4fdfda98960cbc1ac792fa127d51
SwordShieldMouse <alke8882000@gmail.com>
2015-04-26 16:33:00 -0600

    placeholders should be there now

---
+++ Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutOfBounds : MonoBehaviour {
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Projectile") {
+			// Updates projectile count if the object destroyed is a projectile
+			Destroy(other.gameObject);
+			GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().UpdateProjectiles ();
+		} else if (other.gameObject.tag == "Player") {
+			// Restarts the game if the player dies
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
